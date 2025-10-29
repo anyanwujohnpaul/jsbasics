2c5d53460675d9c011a10e6272457c30b4bc0abf
anyanwujohnpaul <johnpaulanyanwu17@gmail.com>
2025-10-29 23:12:44 +0100

    practice

---
+++ freecodecamp.js
@@ -0,0 +1,130 @@
+// Bracket Notations and Accessing Characters from a string
+
+let greeting = "hello";
+console.log(greeting[1]); // "e"
+console.log(greeting[4]); // "e"
+
+
+
+// To get the last character of a string, it always a best practice to use length
+let name = "John";
+console.log(name[name.length - 1]);
+
+
+
+// Getting Multiple Characters
+
+let operator = "Addition";
+let firstThreeLetterOfOperator = operator[0] + operator[1] + operator[2];
+console.log(firstThreeLetterOfOperator)
+
+
+// Creating NewLines in JavaScript
+// In many programming languages, including JavaScript, you can create 
+// a newline in a string using a special character called an escape sequence. The most common escape sequence for newlines is \n.
+
+
+let poem = "Roses are red,\nViolets are blue,\nJavaScript is fun,\nAnd so are you.";
+console.log(poem);
+
+
+// Escaping Inner Quotes
+
+let statement = "She said, \"Hello!\"";
+console.log(statement); // She said, "Hello!"
+
+
+// Template Literals and String Interpolation
+
+name = "Johnpaul";
+greeting = `Hello, ${name}!`;
+
+console.log(greeting);
+
+
+// Using String Concatenation and + operator
+name = "Chinedu";
+age = 25;
+message = "My name is " + name + " and I am " + age + " years old.";
+console.log(message);
+
+// Using Template Literals and String Interpolation
+
+name = "Mubarak";
+age = 25;
+message = `My name is ${name} and I am ${age} years old.`;
+console.log(message);
+
+// Template Literals supports Multiline string
+
+poem = `Roses are red,
+Violets are blue,
+JavaScript is fun,
+And so are you.`;
+
+console.log(poem);
+
+
+// Finding the position of a substring in a string
+
+// A substring is a sequence of characters that appears within a larger string. For example, in the string hello world, hello and world are substrings.
+
+// To locate the position of a substring inside of a string, you can use the indexOf() method.
+// The indexOf() method in JavaScript allows you to search for a substring within a string.
+
+// If the substring is found, indexOf() returns the index (or position) of the first occurrence of that substring. 
+// If the substring is not found, indexOf() returns -1, which indicates that the search was unsuccessful.
+
+
+// Substring is found here
+let sentence = "JavaScript is awesome!";
+let position = sentence.indexOf("awesome!");
+console.log(position); // 14
+
+// the word awesome starts at index 14 in the string JavaScript is awesome!, so the indexOf() method returns 14
+
+
+// Exercises using indexOf
+
+let phrase = "I am going to be a top 1% software Engineer in the world";
+let phrasePosition = phrase.indexOf("going");
+console.log(phrasePosition);
+
+let love_letter = "I love love";
+let loveletter_position = love_letter.indexOf("ove");
+console.log(loveletter_position);
+
+
+// Substring is not found here
+
+phrase = "I am going to be a top 1% software Engineer in the world";
+phrasePosition = phrase.indexOf("believe");
+console.log(phrasePosition);
+// This return -1 because substring could not be found.
+
+
+// You can also specify where to begin searching within the string by providing a second argument to indexOf()
+
+sentence = "JavaScript is awesome, and JavaScript is powerful!";
+position = sentence.indexOf("JavaScript", 10);
+console.log(position); // 27
+
+// Exercise
+
+love_letter = "I love love";
+loveletter_position = love_letter.indexOf("love", 5);
+console.log(loveletter_position);
+
+
+// Short way of finding substring in a string and printing it in the console
+// This returns 1 because it cant be found
+console.log("freeCodeCamp".indexOf("F")) // -1
+
+
+// The Prompt() Method and How it Works
+
+// prompt(message, default);
+// prompt("What is your name?", "Guest");
+
+
+
